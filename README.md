6f326b5934113c767098cbbc0615ea32d0c93491
Fredmjr <fredmjr@gmail.com>
2025-09-10 22:05:33 +0200

    auto profiler & click profiler mgs functions added

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ CURRENT FEATURES:
 - CRUD user and messages.
 
 NOTE: More features to be added soon, like groups. repo also uses esbuild as a client file compressor (buildapp in package.json does the script building & nodemon). Exculte for application to start running: npm run buildapp
+
+NOTE: Project uses a lot of mutation observers(changes in page or element), delegations (tracking child by its parent element) & esbuild's rule sets(e.g no function names)
--- public/src/js/messages.js
+++ public/src/js/messages.js
@@ -1,3 +1,4 @@
+//1. AUTO PROFILER FUNCTION
 // Profiler with mgs writer to db function
 (Fuc = () => {
   const Home = document.querySelector(".Home");
@@ -13,6 +14,7 @@
           Home.addEventListener("click", function (event) {
             if (event.target.matches(".usrcl")) {
               console.log("user button was clicked!");
+              //1.PROFILER MGS
               //mnchtMgs  Function to find message div for messages purpose!
               const mnchtMgsobsrvr = new MutationObserver((mutations) => {
                 mutations.forEach((mutation) => {
@@ -21,12 +23,16 @@
                       ? node
                       : node.querySelector?.(".mnchtMgs");
 
+                    const nodechtsbmtBtn = node.matches?.(".chtsbmtBtn")
+                      ? node
+                      : node.querySelector?.(".chtsbmtBtn");
+
                     if (nodemnchtMgs) {
                       console.log("we have message div");
                       //LOAD SPINNER
                       nodemnchtMgs.innerHTML = `<div>
-      <img src="/assests/animations/loading.gif" width="50" alt="" />
-    </div>`;
+                  <img src="/assests/animations/loading.gif" width="50" alt="" />
+                    </div>`;
                       //COOKIE RETREIVAL...
                       autockie = (elem) => {
                         let ckies = document.cookie.split("; ");
@@ -66,6 +72,93 @@
                           }
                         })
                         .catch((error) => console.log(error));
+                    }
+                  });
+                });
+              });
+
+              mnchtMgsobsrvr.observe(Home, { childList: true, subtree: true });
+            }
+          });
+        }
+      });
+    });
+  });
+
+  proflrobsrvr.observe(Home, { childList: true, subtree: true });
+})();
+
+//
+(Cuc = () => {
+  const Home = document.querySelector(".Home");
+  //to find usrcl the button for search users
+  const proflrobsrvr = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        const nodeusrcl = node.matches?.(".usrcl")
+          ? node
+          : node.querySelector?.(".usrcl");
+
+        if (nodeusrcl) {
+          Home.addEventListener("click", function (event) {
+            if (event.target.matches(".usrcl")) {
+              console.log("user button was clicked!");
+              //1.PROFILER MGS
+              //mnchtMgs  Function to find message div for messages purpose!
+              const mnchtMgsobsrvr = new MutationObserver((mutations) => {
+                mutations.forEach((mutation) => {
+                  mutation.addedNodes.forEach((node) => {
+                    const nodemnchtMgs = node.matches?.(".mnchtMgs")
+                      ? node
+                      : node.querySelector?.(".mnchtMgs");
+
+                    const nodechtsbmtBtn = node.matches?.(".chtsbmtBtn")
+                      ? node
+                      : node.querySelector?.(".chtsbmtBtn");
+
+                    if (nodechtsbmtBtn) {
+                      console.log("we have message div nodechtsbmtBtn");
+                      nodechtsbmtBtn.addEventListener("click", () => {
+                        autockie = (elem) => {
+                          let ckies = document.cookie.split("; ");
+                          for (let i = 0; i < ckies.length; i++) {
+                            let cookie = ckies[i];
+                            let [name, value] = cookie.split("=");
+                            if (name === elem) {
+                              return decodeURIComponent(value);
+                            }
+                          }
+                          return null;
+                        };
+
+                        let sltdusrp = autockie("targtdusrprt");
+                        let lgrusrp = autockie("usrP");
+                        const prtsObj = {
+                          lgrsur: lgrusrp,
+                          sltdusr: sltdusrp,
+                        };
+                        console.log(
+                          `both loger: ${lgrusrp} & recepient: ${sltdusrp}`
+                        );
+
+                        fetch("/mgs/prtsmgs", {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/json",
+                            // 'Authorization': 'Bearer YOUR_TOKEN',
+                          },
+                          body: JSON.stringify(prtsObj),
+                        })
+                          .then((response) => response.text())
+                          .then((data) => {
+                            if (data) {
+                              //Messages
+                              nodemnchtMgs.innerHTML = `${data}`;
+                              console.log(data);
+                            }
+                          })
+                          .catch((error) => console.log(error));
+                      });
                     }
                   });
                 });
